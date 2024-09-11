93a357c6efaa02dbbf27de6d42e9d51015d5b34a
Delta Engine <nikolateslamc@gmail.com>
2024-09-11 18:22:47 +0200

    hidden env token

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
+import {config} from 'dotenv';
 import { SapphireClient } from '@sapphire/framework';
 import { GatewayIntentBits } from 'discord.js';
+import { configDotenv } from 'dotenv';
+
+config();
 
 const client = new SapphireClient({
   intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
   loadMessageCommandListeners: true
 });
+
+const TOKEN = process.env.TOKEN;
+client.login(TOKEN);
